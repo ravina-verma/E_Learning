ce62de9bf2b28ae051195cd7129937a028d2df65
Yogesh-Mujawdiya <ykmujawdiya@gmail.com>
2020-08-21 18:03:08 +0530

    Initial commit

--- E_Learning/settings.py
+++ E_Learning/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 BASE_DIR1 = os.path.dirname(os.path.dirname(__file__))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'E_Learning.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR1, 'db.sqlite3'),
     }
 }
 
